5dcbfce69074313a4e475620b6f14d67ab257c10
Sravanthi Gudimalla <gudimalla@fb.com>
2022-09-16 11:08:31 -0700

    Varibles in log files

--- Frontend.sh
+++ Frontend.sh
@@ -1,9 +1,10 @@
+LOG_FILE=/tmp/frontend
 echo installing Nginx
-yum install nginx -y &>>/tmp/frontend
+yum install nginx -y &>>LOG_FILE
 echo status =$?
 
 echo downloading Nginx Web content
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"&>>/tmp/frontend
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"&>>LOG_FILE
 echo status =$?
 
 cd /usr/share/nginx/html
@@ -12,13 +13,13 @@ echo removing old content
 rm -rf * &>>/tmp/frontend
 
 echo extracting web content
-unzip /tmp/frontend.zip &>>/tmp/frontend
+unzip /tmp/frontend.zip &>>LOG_FILE
 echo status =$?
 
-mv frontend-main/static/* &>>/tmp/frontend
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+mv frontend-main/static/* &>>LOG_FILE
+mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf &>>LOG_FILE
 
 echo starting nginx service
-systemctl enable nginx &>>/tmp/frontend
-systemctl restart nginx &>>/tmp/frontend
+systemctl enable nginx &>>LOG_FILE
+systemctl restart nginx &>>LOG_FILE
 echo status =$?
