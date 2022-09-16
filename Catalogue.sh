4cea003aa578facd5a6bca04029cf1c7a36f2322
Sravanthi Gudimalla <gudimalla@fb.com>
2022-09-16 15:14:00 -0700

    Catalogue

--- Catalogue.sh
+++ Catalogue.sh
@@ -16,12 +16,13 @@ echo "Download Catalogue Application code"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>${LOG_FILE}
 echo status = $?
 
+
+
 echo "Extract Application Catalogue code"
 unzip /tmp/catalogue.zip &>>${LOG_FILE}
 echo status = $?
 
 mv catalogue-main catalogue
-cd /home/roboshop/catalogue
 
 echo "Install Nodejs dependencies"
 npm install &>>${LOG_FILE}
@@ -31,9 +32,9 @@ echo "setup catalogue service"
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service &>>${LOG_FILE}
 echo status = $?
 
-systemctl daemon-reload &>>${LOG_FILE}
+systemctl daemon-reload catalogue &>>${LOG_FILE}
 systemctl enable catalogue &>>${LOG_FILE}
 
 echo "start catalogue service"
-systemctl start catalogue &>>${LOG_FILE}
+systemctl restart catalogue &>>${LOG_FILE}
 echo status = $?
